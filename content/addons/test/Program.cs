4c8597c0e71958e18af12d6aae83d22776a3edc0
Alex <alex@gu3.me>
2021-09-12 13:24:25 +0100

    C# to rust callbacks Macro helpers for strings

--- content/addons/test/Program.cs
+++ content/addons/test/Program.cs
@@ -1,14 +1,33 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
-namespace Test {
-    public static class Program {
-        public static int Hello(IntPtr args, int sizeBytes) {
-            Console.WriteLine("Hello from C#!");
-            return 42;
-        }
+namespace Test
+{
+    [StructLayout(LayoutKind.Sequential)]
+    struct Test
+    {
+        public IntPtr name;
+        public int age;
+        public IntPtr callback;
+    }
 
-        public void Update() {
+    public delegate void Callback([MarshalAs(UnmanagedType.LPStr)] string str);
 
+    public static class Program
+    {
+        public static int Hello(IntPtr args, int sizeBytes)
+        {
+            var test = Marshal.PtrToStructure<Test>(args);
+
+            var nameStr = Marshal.PtrToStringUTF8(test.name);
+
+            Console.WriteLine($"Name: {nameStr}");
+            Console.WriteLine($"Age: {test.age}");
+
+            var callbackTestFn = Marshal.GetDelegateForFunctionPointer<Callback>(test.callback);
+            callbackTestFn.Invoke("8======D hehe");
+
+            return test.age;
         }
     }
 }
--- src/scripting/script_manager.rs
+++ src/scripting/script_manager.rs
@@ -1,7 +1,3 @@
-use std::thread;
-
-use netcorehost::{nethost, pdcstr};
-
 // ============================================================================
 //
 // script_manager.rs
@@ -10,22 +6,52 @@ use netcorehost::{nethost, pdcstr};
 //
 // ============================================================================
 
+use netcorehost::{nethost, pdcstr};
+use std::{
+    ffi::{c_void, CStr, CString},
+    os::raw::c_char,
+    thread,
+};
+
+macro_rules! csharp_str {
+    ($s:expr) => {
+        CString::new($s).unwrap().into_raw()
+    };
+}
+
+macro_rules! csharp_str_to_string {
+    ($s:expr) => {
+        unsafe { CStr::from_ptr($s as *const c_char).to_str().unwrap() }
+    };
+}
+
 pub struct ScriptManager {}
+
+#[repr(C)]
+struct Test {
+    pub name: *mut c_char,
+    pub age: i32,
+    pub callback: extern "C" fn(arg: *mut c_void),
+}
+
+extern "C" fn callback(arg: *mut c_void) {
+    println!("stringy: {}", csharp_str_to_string!(arg));
+}
 
 impl ScriptManager {
     pub fn new() -> ScriptManager {
         thread::Builder::new()
-            .name("dotnet thread".to_string())
+            .name(".NET Scripting thread".to_string())
             .spawn(|| 'thread: loop {
                 let hostfxr = nethost::load_hostfxr().unwrap();
                 let context = hostfxr
                     .initialize_for_runtime_config(pdcstr!(
-                        "content/addons/test/bin/Release/net6.0/win-x64/test.runtimeconfig.json"
+                        "content/addons/test/bin/Release/net5.0/win-x64/test.runtimeconfig.json"
                     ))
                     .unwrap();
                 let fn_loader = context
                     .get_delegate_loader_for_assembly(pdcstr!(
-                        "content/addons/test/bin/Release/net6.0/win-x64/test.dll"
+                        "content/addons/test/bin/Release/net5.0/win-x64/test.dll"
                     ))
                     .unwrap();
                 let hello = fn_loader
@@ -34,7 +60,13 @@ impl ScriptManager {
                         pdcstr!("Hello"),
                     )
                     .unwrap();
-                let result = unsafe { hello(std::ptr::null(), 0) };
+
+                let test = Test {
+                    name: csharp_str!("Alex"),
+                    age: 32,
+                    callback: callback,
+                };
+                let result = unsafe { hello(&test as *const Test as *const c_void, 0) };
                 println!("C# exited with {}", result);
 
                 break 'thread;
